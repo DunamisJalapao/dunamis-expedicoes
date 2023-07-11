79cbb1ce4307bb25a076fb574272ae1d407126fd
João Vitor Soares <joaovitorsoares12@gmail.com>
2023-07-11 00:24:58 -0300

    Fix: Corrigindo zindex do botão do whatsapp

--- src/components/Screens/Places.tsx
+++ src/components/Screens/Places.tsx
@@ -12,7 +12,7 @@ export default async function Places({ ...rest }: PackType) {
   return (
     <div {...rest} className="w-full md:w-screen h-full bg-white">
       <div className="flex w-full h-full py-5 md:py-[4rem] justify-center flex-col gap-8">
-        <h1 className="text-center  text-2xl md:text-5xl text-[#112126ff] font-bold font-lato">Confira o que te <br /> AGUARDA!</h1>
+        <h1 className="text-center text-2xl md:text-5xl text-[#112126ff] font-bold font-lato">Confira o que te <br /> AGUARDA!</h1>
         <div className="flex flex-col gap-2">
           <p className="text-center uppercase text-lg md:text-2xl">Nosso instagram</p>
           <div className="grid w-full grid-cols-resposive-fit-two justify-center gap-10">

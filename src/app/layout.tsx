e2c0ed12e33f1a99ee502a5694f9a4e8d2857ed4
João Vitor Soares <joaovitorsoares12@gmail.com>
2023-08-25 10:56:52 -0300

    Fix: Tag analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,25 +77,30 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <Script>
-          {`
-            (function (w, d, s, l, i) {
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function (w, d, s, l, i) {
               w[l] = w[l] || []; w[l].push({
                 'gtm.start':
                   new Date().getTime(), event: 'gtm.js'
               }); var f = d.getElementsByTagName(s)[0],
                 j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
                   'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', 'GTM-W8PHTL9X');
+            })(window, document, 'script', 'dataLayer', 'GTM-W8PHTL9X');`}}>
+
+        </Script>
+        <Script>
+          {`
+            
           `}
         </Script>
 
       </head>
       <body className={`${lato.variable} ${lemonMilk.variable}`}>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8PHTL9X"
-            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
-        </noscript>
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8PHTL9X"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>`}} />
         <Providers>
           {children}
         </Providers>
